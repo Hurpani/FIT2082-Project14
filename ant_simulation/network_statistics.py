d3b21e7cd880d2f9e01f71541bf2eb697654b492
robert <granchrobert@gmail.com>
2020-10-14 18:41:56 +1100

    added random graph support for statistics

--- ant_simulation/network_statistics.py
+++ ant_simulation/network_statistics.py
@@ -2,6 +2,36 @@ import matplotlib.pyplot as plt
 import networkx as nx
 import collections
 import numpy as np
+import random as random
+
+def create_random_graph_based_off(G):
+    #create a random graph with same number of nodes and edges
+    F = nx.generators.random_graphs.gnm_random_graph(G.number_of_nodes(),G.number_of_edges())
+
+    #work out total weight of edges in base graph
+    total_weight = 0
+    for edge in list(G.edges.data()):
+        total_weight += int(edge[2]['weight'])
+
+    #assign each edge with a weight of 1 and take that away from the total weight amount
+    total_weight -= F.number_of_nodes()
+    for edge in list(F.edges.data()):
+        edge[2]['weight'] = 1
+
+    #loop though edges 1% of the time adding a weight to that edge and de-incrementing total_weight
+    #repeated until no more weights to add
+    while total_weight > 0:
+        for edge in list(F.edges.data()):
+            if total_weight == 0:
+                break
+
+            if random.random() < 0.01:
+                edge[2]['weight'] += 1
+                total_weight -= 1
+
+    return F
+
+
 
 def remove_edge_weights_less_than(G, amount:int = 0):
     for edge in list(G.edges.data()):
@@ -46,6 +76,10 @@ def community_size_ratio(G):
         List[i] = int(List[i])
         List[i] /= 1000
     print(List)
+    plt.bar([0,1,2],List , width=0.80, color="b")
+    plt.title("Community Size Comparision ")
+    plt.ylabel("percentage of nodes in each community")
+    plt.show()
     return List
 
 def degree_distribution(G):
@@ -90,23 +124,37 @@ def showGraph(G):
     plt.show()
 if __name__ == "__main__":
 
+    #Random
+    colony = 5
+    day = 1
+    G = nx.read_graphml("C:/Users/Desktop/FIT2082/6ant/Ant_Keller/weighted_network_col" + str(colony) + "_day" + str(day) + ".graphml")
+    F = create_random_graph_based_off(G)
+    remove_edge_weights_less_than(F, 15)
+    Fc = F.subgraph(max(nx.connected_components(F))).copy()
+    community_size_ratio(Fc)
+    degree_distribution(Fc)
+    eccentricity_l(Fc)
+    showGraph(Fc)
+
+    #Simulation
     file = open("C:/Users/Desktop/FIT2082/FIT2082-Project14/edge_list.txt", "rb")
     G = nx.read_edgelist(file)
     file.close()
-
     remove_edge_weights_less_than(G,800)
     community_size_ratio(G)
     degree_distribution(G)
     eccentricity_l(G)
     showGraph(G)
 
-
-    G = nx.read_graphml("C:/Users/Desktop/FIT2082/6ant/Ant_Keller/weighted_network_col5_day" + "30" + ".graphml")
-
+    #real world
+    colony = 5
+    day = 1
+    G = nx.read_graphml("C:/Users/Desktop/FIT2082/6ant/Ant_Keller/weighted_network_col" + str(colony) + "_day" + str(day) + ".graphml")
     remove_edge_weights_less_than(G, 20)
     community_size_ratio(G)
     degree_distribution(G)
     eccentricity_l(G)
     showGraph(G)
+
 
 
