ed8ddf96aeac9e234553ac7b833e653c7bc6e6b1
Hurpani <henri.urpani@gmail.com>
2021-07-12 14:40:05 +1000

    Set up system for interaction count calibration

--- program.py
+++ program.py
@@ -3,19 +3,28 @@ from __future__ import annotations
 ##############################
 from ant_simulation.analysis.automated_testing import batch_test
 from ant_simulation.analysis.analyse import analyse_in
+import networkx as nx
+from ant_simulation.analysis.graph_analysis import display_network
 
 
 def perform_testing() -> None:
     # Example batch:
-    batch_test("demo_test", 2, [("output.txt", "actors.txt", "world_objects.txt", 500)],
-               [{"testing_scale": 1.0, "pheromone_testing_scale": 1.0, "interacting_ticks_threshold": 3},
-                {"testing_scale": 10.0, "pheromone_testing_scale": 2.0, "interacting_ticks_threshold": 3}])
+    batch_test("tests", 5, [("output.txt", "actors.txt", "world_objects.txt", 86400)],
+               [{"testing_scale": 1.0, "pheromone_testing_scale": 1.0, "interacting_ticks_threshold": 1},
+                {"testing_scale": 1.0, "pheromone_testing_scale": 1.0, "interacting_ticks_threshold": 2},
+                {"testing_scale": 1.0, "pheromone_testing_scale": 1.0, "interacting_ticks_threshold": 3},
+                {"testing_scale": 1.0, "pheromone_testing_scale": 1.0, "interacting_ticks_threshold": 4},
+                {"testing_scale": 1.0, "pheromone_testing_scale": 1.0, "interacting_ticks_threshold": 5}])
 
 
 def perform_analysis() -> None:
-    analyse_in("./network_outputs/demo_test/", "./network_outputs/demo_analyses")
+    analyse_in("./network_outputs/tests/", "./network_outputs/analyses")
 
 
 if __name__ == "__main__":
+    # display_network(nx.read_graphml("./network_outputs/demo_test/network_0-1(2).gml"))
+
+    perform_testing()
+
     print("Hello, World! (todo: request command-line inputs from a user)")
 
